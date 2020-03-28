28112ba6c74402dde8a0409841534e3277c602b6
JT <foosyou@gmail.com>
2020-03-28 18:24:10 -0500

    high level overview

---
+++ dot1x_applier.py
@@ -0,0 +1,60 @@
+#!/usr/local/bin/python3
+'''
+This script is to create a report on switches with dot1x enabled
+'''
+
+from nornir import InitNornir
+from nornir.core.filter import F
+from nornir.plugins.functions.text import print_result
+from nornir.plugins.tasks.networking import netmiko_send_command
+from pprint import pprint as pp
+from ttp import ttp
+
+
+# Get interfaces
+def get_interfaces(task):
+
+    print(f'{task.host}: checking dot1x status.')
+    # run "show dot1x all" on each host
+    sh_dot1x = task.run(
+        task=netmiko_send_command,
+        command_string="show dot1x all",
+    )
+
+    # TTP template for dot1x status
+    dot1x_ttp_template = "Sysauthcontrol              {{ status }}"
+
+    # magic TTP parsing
+    parser = ttp(data=sh_dot1x.result, template=dot1x_ttp_template)
+    parser.parse()
+    dot1x_status = json.loads(parser.result(format='json')[0])
+
+    print(f"{task.host}: {dot1x_status[0]['status']}")
+    return dot1x_status[0]['status']
+
+
+# Apply global dot1x config template
+def apply_global_dot1x(task):
+    _stuff = None
+
+
+# Apply interface dot1x config templates
+def apply_int_dot1x(task):
+    _stuff = None
+
+
+def main():
+    # initialize The Norn
+    nr = InitNornir()
+    # filter The Norn
+    nr = nr.filter(platform="cisco_ios")
+    # run The Norn to get interfaces
+    nr.run(task=get_interfaces)
+    # run The Norn to apply global dot1x config
+    nr.run(task=apply_global_dot1x)
+    # run The Norn to apply interface dot1x config
+    nr.run(task=apply_int_dot1x)
+
+
+if __name__ == "__main__":
+    main()
