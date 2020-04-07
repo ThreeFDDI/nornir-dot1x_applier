596ece22ff1275221f29a4cabeb0d56d77d86ab8
JT <foosyou@gmail.com>
2020-04-06 19:11:35 -0500

    comments, banner printing, ibns detection moved, write/apply

--- dot1x_applier.py
+++ dot1x_applier.py
@@ -1,6 +1,28 @@
 #!/usr/local/bin/python3
 '''
-This script is to apply dot1x config to Catalyst switch stacks
+
+This script uses the Nornir framework to apply dot1x config to Catalyst switches.
+
+IBNS version 1 or 2 templates will be applied based on switch model. Catalyst
+3750 series switches will recieve IBNS version 1 and all other switch models
+will receive IBNS version 2. 
+
+Additional variables required from SimepleInventory:
+
+ise_key:            ISE RADIUS key
+ise_vip_a_name:     ISE cluster A hostname
+ise_vip_a_ip:       ISE cluster A VIP ip address
+ise_vip_a_psn1:     ISE cluster A PSN1 ip address
+ise_vip_a_psn2:     ISE cluster A PSN2 ip address
+ise_vip_b_name:     ISE cluster B hostname
+ise_vip_b_ip:       ISE cluster B VIP ip address
+ise_vip_b_psn1:     ISE cluster B PSN1 ip address
+ise_vip_b_psn2:     ISE cluster B PSN2 ip address
+region:             east or west ISE region
+vlans:              list of vlans dot1x will be applied to
+uplinks:            list of ports that will recieve dot1x uplink config
+excluded_intf:      list of ports that will be excluded from dot1x 
+
 '''
 
 from nornir import InitNornir
@@ -9,8 +31,16 @@ from nornir.plugins.tasks.networking import netmiko_send_config
 from nornir.plugins.tasks import text
 
 
+def c_print(printme):
+    # Print centered text with newline before and after
+    print(f"\n" + printme.center(80, ' ') + "\n")
+
+
 # Get info from switches
 def get_info(task):
+
+    c_print('Gathering device configurations')
+    
 
     # get software version; use TextFSM
     sh_version = task.run(
@@ -35,13 +65,26 @@ def get_info(task):
 
     # save interfaces to task.host
     task.host['intfs'] = interfaces.result
-    
+
+    print("\n" + '~'*80)    
+
+        # choose template based on switch model
+    if "3750" in task.host['sw_model']:
+        # 3750's use IBNSv1
+        task.host['ibns_ver'] = 'v1'
+        c_print('IBNS version 1 detected')
+    else:
+        # all else use IBNSv2
+        task.host['ibns_ver'] = 'v2'
+        c_print('IBNS version 1 detected')
+
+    print("\n" + '~'*80)    
 
 # IBNS global config templates
-def ibns_global(task, ibns_ver):
+def ibns_global(task):
     global_cfg = task.run(
         task=text.template_file, 
-        template=f"IBNS{ibns_ver}_{task.host['region']}_global.j2", 
+        template=f"IBNS{task.host['ibns_ver']}_{task.host['region']}_global.j2",
         path="templates/", 
         **task.host
     )
@@ -50,7 +93,7 @@ def ibns_global(task, ibns_ver):
 
 
 # IBNS interface config templates
-def ibns_intf(task, ibns_ver):
+def ibns_intf(task):
     
     # init lists of interfaces
     access_interfaces = []
@@ -83,7 +126,7 @@ def ibns_intf(task, ibns_ver):
     # render access interface configs
     access_intf_cfg = task.run(
         task=text.template_file, 
-        template=f"IBNS{ibns_ver}_access_intf.j2", 
+        template=f"IBNS{task.host['ibns_ver']}_access_intf.j2", 
         path="templates/", 
         **task.host
     )
@@ -104,40 +147,30 @@ def render_configs(task):
     # create vlan_list string
     task.host['vlan_list'] = ",".join(task.host['vlans'])
 
-    # choose template based on switch model
-    if "3750" in task.host['sw_model']:
-        # 3750's use IBNSv1
-        ibns_ver = 'v1'
-    else:
-        # all else use IBNSv2
-        ibns_ver = 'v2'
 
     # function to render global configs
-    global_cfg = ibns_global(
-        task,
-        ibns_ver,
-    )
+    global_cfg = ibns_global(task)
 
     # function to run interface configs
-    intf_cfg = ibns_intf(
-        task,
-        ibns_ver,
-    )
+    intf_cfg = ibns_intf(task)
 
-    print(global_cfg + "\n" + intf_cfg)
+    task.host['cfg_out'] = global_cfg + "\n" + intf_cfg
 
+
+# write switch configs
+def write_configs(task):
+    # write config file
+    with open(f"configs/{task.host}_dot1x.txt", "w+") as f:
+        f.write(task.host['cfg_out'])
 
 # apply switch configs
 def apply_configs(task):
-    print(task.host['region'])
-    print(task.host['ise_vip_a_name'])
-    print(task.host['ise_vip_a_ip'])
-    print(task.host['ise_vip_a_psn1'])
-    print(task.host['ise_vip_a_psn2'])
-    print(task.host['ise_vip_b_name'])
-    print(task.host['ise_vip_b_ip'])
-    print(task.host['ise_vip_b_psn1'])
-    print(task.host['ise_vip_b_psn2'])
+
+    task.run(
+        task=netmiko_send_config, 
+        config_file=f"configs/{task.host}_dot1x.txt"
+    )
+
 
 
 # main function
@@ -151,7 +184,9 @@ def main():
     # run The Norn to apply dot1x config
     nr.run(task=render_configs)
 
-    nr.run(task=apply_configs)
+    nr.run(task=write_configs)
+
+    #nr.run(task=apply_configs)
 
 
 if __name__ == "__main__":
