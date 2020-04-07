055a38b067c76774221de4bc247dea3842d12e20
JT <foosyou@gmail.com>
2020-04-06 21:30:28 -0500

    comments, sample inventory

--- dot1x_applier.py
+++ dot1x_applier.py
@@ -3,11 +3,11 @@
 
 This script uses the Nornir framework to apply dot1x config to Catalyst switches.
 
-IBNS version 1 or 2 templates will be applied based on switch model. Catalyst
-3750 series switches will recieve IBNS version 1 and all other switch models
-will receive IBNS version 2. 
+Cisco IBNS version 1 or 2 templates will be applied based on switch model. 
+Catalyst 3750 series switches will receive IBNS version 1 and all other 
+switch models will receive IBNS version 2. 
 
-Additional variables required from SimepleInventory:
+Additional variables required from SimpleInventory:
 
 ise_key:            ISE RADIUS key
 ise_vip_a_name:     ISE cluster A hostname
@@ -25,22 +25,42 @@ excluded_intf:      list of ports that will be excluded from dot1x
 
 '''
 
+from getpass import getpass
 from nornir import InitNornir
 from nornir.plugins.tasks.networking import netmiko_send_command
 from nornir.plugins.tasks.networking import netmiko_send_config
 from nornir.plugins.tasks import text
 
 
+# print formatting function
 def c_print(printme):
     # Print centered text with newline before and after
     print(f"\n" + printme.center(80, ' ') + "\n")
 
 
-# Get info from switches
-def get_info(task):
+# set device credentials
+def nornir_set_creds(norn, username=None, password=None):
+    # print banner
+    print()
+    print('~'*80)
+    c_print('Check inventory for device credentials')
 
+    for host_obj in norn.inventory.hosts.values():
+        
+        if not host_obj.username:
+            c_print('Please enter device credentials:')
+            username = input("Username: ")
+            host_obj.username = username
+            password = getpass()
+            host_obj.password = password
+
+    print('\n' + '~'*80)
+
+
+# get info from switches
+def get_info(task):
+    # print banner
     c_print('Gathering device configurations')
-    
 
     # get software version; use TextFSM
     sh_version = task.run(
@@ -48,7 +68,6 @@ def get_info(task):
         command_string="show version",
         use_textfsm=True,
     )
-
     # save show version output to task.host
     task.host['sh_version'] = sh_version.result[0]
     # pull model from show version
@@ -62,26 +81,26 @@ def get_info(task):
         command_string="show interface switchport",
         use_textfsm=True,
     )
-
     # save interfaces to task.host
     task.host['intfs'] = interfaces.result
 
-    print("\n" + '~'*80)    
-
-        # choose template based on switch model
+    # choose template based on switch model
     if "3750" in task.host['sw_model']:
         # 3750's use IBNSv1
         task.host['ibns_ver'] = 'v1'
-        c_print('IBNS version 1 detected')
+        c_print('*** IBNS version 1 detected ***')
     else:
         # all else use IBNSv2
         task.host['ibns_ver'] = 'v2'
-        c_print('IBNS version 1 detected')
+        c_print('*** IBNS version 1 detected ***')
 
-    print("\n" + '~'*80)    
+    print('~'*80)    
 
-# IBNS global config templates
+
+# render IBNS global config templates
 def ibns_global(task):
+    # print banner
+    c_print(f"Rendering IBNS{task.host['ibns_ver']} global configurations")
     # render global configurations
     global_cfg = task.run(
         task=text.template_file, 
@@ -89,12 +108,15 @@ def ibns_global(task):
         path="templates/", 
         **task.host
     )
+    print('~'*80)    
     # return configuration
     return global_cfg.result
 
 
 # IBNS interface config templates
 def ibns_intf(task):
+    # print banner
+    c_print(f"Rendering IBNS{task.host['ibns_ver']} interface configurations")
     # init lists of interfaces
     access_interfaces = []
     uplink_interfaces = []
@@ -128,6 +150,7 @@ def ibns_intf(task):
         path="templates/", 
         **task.host
     )
+    print('~'*80)    
     # return configuration
     return uplink_intf_cfg.result + access_intf_cfg.result
 
@@ -152,18 +175,24 @@ def render_configs(task):
 
 # write switch configs
 def write_configs(task):
+    # print banner
+    c_print(f"Writing IBNS{task.host['ibns_ver']} configuration files to disk")
     # write config file for each host
     with open(f"configs/{task.host}_dot1x.txt", "w+") as f:
         f.write(task.host['cfg_out'])
+    print('~'*80)    
 
 
 # apply switch configs
 def apply_configs(task):
+    # print banner
+    c_print(f"Applying IBNS{task.host['ibns_ver']} configuration files to devices")
     # apply config file for each host
     task.run(
         task=netmiko_send_config, 
         config_file=f"configs/{task.host}_dot1x.txt"
     )
+    print('~'*80)    
 
 
 # main function
@@ -172,14 +201,16 @@ def main():
     nr = InitNornir()
     # filter The Norn
     nr = nr.filter(platform="cisco_ios")
+    # check The Norn for credentials
+    nornir_set_creds(nr)
     # run The Norn to get info
     nr.run(task=get_info)
-    # run The Norn to apply dot1x config
+    # run The Norn to render dot1x config
     nr.run(task=render_configs)
     # run The Norn to write config files
     nr.run(task=write_configs)
     # run The Norn to apply config files
-    #nr.run(task=apply_configs)
+    nr.run(task=apply_configs)
 
 
 if __name__ == "__main__":
